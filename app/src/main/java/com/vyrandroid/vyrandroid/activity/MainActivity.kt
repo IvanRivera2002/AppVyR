59164fa4e2d661b00f89fc15f6ff87575b883bdf
prueba2024 <app01@vyrgs.com>
2024-03-08 16:58:45 -0500

    avances del bottom navigation

--- app/src/main/java/com/vyrandroid/vyrandroid/activity/MainActivity.kt
+++ app/src/main/java/com/vyrandroid/vyrandroid/activity/MainActivity.kt
@@ -5,17 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vyrandroid.vyrandroid.R
+import com.vyrandroid.vyrandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+
 }
