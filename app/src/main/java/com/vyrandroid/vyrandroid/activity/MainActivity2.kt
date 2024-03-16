1063d4f830bb92f18d73428e4349783a17cc86c5
IvanRivera2002 <carlosivanriveraespilco@gmail.com>
2024-03-16 13:57:22 -0500

    cambio el agp a 2.2.2 por compatabilidad y borrar el ides.xml

---
+++ app/src/main/java/com/vyrandroid/vyrandroid/activity/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.vyrandroid.vyrandroid.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vyrandroid.vyrandroid.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
